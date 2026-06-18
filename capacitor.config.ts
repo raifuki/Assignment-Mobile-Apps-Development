446df54fa29d501e21273faa25445319cf99f60c
Raifuki <raifukiayuei@gmail.com>
2026-06-18 20:22:28 +0700

    Add project files

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'App name: Kotlin QuizPackage ID: com.raifuki.kotlinquiz',
+  webDir: 'dist'
+};
+
+export default config;
